25e5aac7f6149352b78ff9673ea77c589a77044b
ChiaraUsa <chiaravivianvalo@gmail.com>
2024-07-20 13:12:35 -0500

    Intentos por solucionar error de conexión

--- backend/config/gemini.js
+++ backend/config/gemini.js
@@ -2,4 +2,8 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.API_KEY);
 
+if (!API_KEY) {
+    throw new Error("API_KEY no está definida en el entorno");
+}
+
 export default genAI;
